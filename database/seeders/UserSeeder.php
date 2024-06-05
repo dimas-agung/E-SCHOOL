36a65fa28087404a3a6e1650788d3296f1649b99
dimas agung <agungdimas390@gmail.com>
2024-06-05 22:03:12 +0700

    add seeder n sanctum auth api

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        $admin = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@email.com',
+            'password' => Hash::make('admin123'),
+        ]);
+        $teacher = User::create([
+            'name' => 'Teacher',
+            'email' => 'teacher@email.com',
+            'password' => Hash::make('teacher123'),
+        ]);
+        $student = User::create([
+            'name' => 'student',
+            'email' => 'student@email.com',
+            'password' => Hash::make('student123'),
+        ]);
+
+        $admin->assignRole(['admin']);
+        $teacher->assignRole(['teacher']);
+        $student->assignRole(['student']);
+    }
+}
